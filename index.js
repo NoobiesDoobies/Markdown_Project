bcab22156ad573e2db412186093c12e3451342ad
NoobiesDoobies <carlioseryan20@gmail.com>
2022-02-18 12:37:39 +0700

    Routes and EJS

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require("express")
 const app = express()
 const port = 3000;
+const articleRouter = require("./routes/articles")
 
-app.get("", (req, res) =>{
-    console.log("test")
-    res.send("hiiii")
+
+// untuk baca html
+app.set("view engine", "ejs")
+app.set("views", "ejs")
+
+
+
+app.get("/", (req, res) =>{
+    const articles = [{
+        title: "Test Article",
+        createdAt: Date.now(),
+        description: "Test description"
+    }]
+    res.render("html", {articles:articles})
 })
+
+
+app.use("/articles", articleRouter)
 
 app.listen(port, (req, res)=>{
     console.log(`Listening at port ${port}`)
